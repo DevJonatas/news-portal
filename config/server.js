9d99d4c0d51972eb156c218c8467146e0f918f97
DevJonatas <jonatas@workmail.com>
2018-07-07 08:43:01 -0300

    config routes basic

---
+++ config/server.js
@@ -0,0 +1,12 @@
+let express = require('express');
+let app = express();
+let consign = require('consign');
+
+app.set('view engine', 'ejs');
+app.set('views','./app/views');
+
+consign()
+    .include('app/routes')
+    .into(app);
+
+module.exports = app;
